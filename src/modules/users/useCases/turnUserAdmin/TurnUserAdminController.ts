2abe63b900c58f66918fa49559c545bf31a23da0
Almeida - 748 <rafael.lara@cairunet.ad.br>
2021-03-23 12:40:45 -0400

    testes concluidos

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userAttualized = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(userAttualized);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
